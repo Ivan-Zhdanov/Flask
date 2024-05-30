60bebf9896c5075b1c1eadbce50aa9fd83dcffb0
Ivan-Zhdanov <ivan.zh.moscow@mail.ru>
2024-05-30 18:09:14 +0300

    first

---
+++ hello.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    first_name = 'John'
+    pizza = ['Peperoni', '4 cheese', 'American']
+    return render_template("index.html", pizza=pizza)
+
+
+# localhost:5000/users/Ivan
+@app.route('/users/<name>')
+def users(name):
+    return render_template("users.html", user_name=name)
+
+
+# Создание обработки страниц ошибок
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_err(e):
+    return render_template("500.html"), 500
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
